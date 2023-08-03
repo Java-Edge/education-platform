ccd590f3a86c037c8445956bab03bd68eb6d348c
JavaEdge <sss2855845850@gmail.com>
2023-08-03 09:54:35 +0800

    refactor:移除 swagger

--- education-back/src/main/java/com/javagpt/back/response/Result.java
+++ education-back/src/main/java/com/javagpt/back/response/Result.java
@@ -1,13 +1,13 @@
 package com.javagpt.back.response;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
+ * 统一响应对象
+ *
  * @PackageName:com.javagpt.background.result
  * @ClassName:Result
  * @Description:
@@ -18,14 +18,11 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
-@ApiModel("统一响应对象")
 public class Result<T>{
-    @ApiModelProperty("状态码")
+
     private int code;
 
-    @ApiModelProperty("信息")
     private String message;
 
-    @ApiModelProperty("数据")
     private T data;
 }
