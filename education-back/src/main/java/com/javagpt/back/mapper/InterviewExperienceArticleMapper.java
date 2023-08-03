9db53353b4788f8a92e9524290d1e5e2034a6b12
JavaEdge <sss2855845850@gmail.com>
2023-08-03 13:56:37 +0800

    refactor:升级 mp 适配 JDK17。移除不适配的无用的 JWT

--- education-back/src/main/java/com/javagpt/back/mapper/InterviewExperienceArticleMapper.java
+++ education-back/src/main/java/com/javagpt/back/mapper/InterviewExperienceArticleMapper.java
@@ -9,9 +9,6 @@ import com.javagpt.back.vo.ArticleVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import javax.jws.soap.InitParam;
-import java.util.List;
-
 /**
 * @author MSIK
 * @description 针对表【interview_experience_article】的数据库操作Mapper
