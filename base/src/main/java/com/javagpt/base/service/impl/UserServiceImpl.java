01d95043107e9ef4fc2f94868d5cb8e42f643fc5
不败顽童 <2631416434@qq.com>
2023-08-06 21:55:10 +0800

    fix： 修复用户无法注册的问题

--- base/src/main/java/com/javagpt/base/service/impl/UserServiceImpl.java
+++ base/src/main/java/com/javagpt/base/service/impl/UserServiceImpl.java
@@ -4,33 +4,35 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.javagpt.base.dto.UserDTO;
 import com.javagpt.base.entity.UserEntity;
-import com.javagpt.base.service.UserService;
 import com.javagpt.base.mapper.UserMapper;
+import com.javagpt.base.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 /**
-* @author MSIK
-* @description 针对表【user(用户表)】的数据库操作Service实现
-* @createDate 2023-08-05 18:49:02
-*/
+ * @author MSIK
+ * @description 针对表【user(用户表)】的数据库操作Service实现
+ * @createDate 2023-08-05 18:49:02
+ */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserEntity>
-    implements UserService{
+        implements UserService {
 
     @Override
     public boolean chkUsername(String username) {
-
-        return true;
+        UserEntity userEntity = this.getBaseMapper()
+                .selectOne(new LambdaQueryWrapper<UserEntity>()
+                        .eq(UserEntity::getUsername, username));
+        return userEntity != null;
     }
 
     @Override
     public int insertUser(UserDTO user) {
 
         UserEntity userEntity = new UserEntity();
-        BeanUtils.copyProperties(user,userEntity);
+        BeanUtils.copyProperties(user, userEntity);
         userEntity.setCreatTime(new Date());
         userEntity.setUpdateTime(new Date());
 
@@ -42,8 +44,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserEntity>
     public UserEntity selectUser(String username, String password) {
 
         LambdaQueryWrapper<UserEntity> wrapper = new LambdaQueryWrapper<UserEntity>()
-                .eq(UserEntity::getUsername,username)
-                .eq(UserEntity::getPassword,password);
+                .eq(UserEntity::getUsername, username)
+                .eq(UserEntity::getPassword, password);
 
         UserEntity userEntity = this.getBaseMapper().selectOne(wrapper);
         return userEntity;
