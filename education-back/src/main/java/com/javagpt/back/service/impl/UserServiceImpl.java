efa9ddcc5b184fe528a8ae3bb8a06dccd1bd7a98
不败顽童 <2631416434@qq.com>
2023-08-17 23:46:58 +0800

    fix: 加入userId

--- education-back/src/main/java/com/javagpt/back/service/impl/UserServiceImpl.java
+++ education-back/src/main/java/com/javagpt/back/service/impl/UserServiceImpl.java
@@ -129,7 +129,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserEntity>
 
         String token = builder.setSubject(userEntity.getUsername())                     //载荷部分，主题，就是token中携带的数据，这里把用户名放进去
                 .setIssuedAt(new Date())                            //设置token的生成时间
-                .setId(user.getId())               //设置用户id为token  id      ''是因为用户id是int类型，需要转换为字符串类型
+                .setId(userEntity.getId())               //设置用户id为token  id      ''是因为用户id是int类型，需要转换为字符串类型
                 .setClaims(map)                                     //map中可以存放用户的角色权限信息
                 .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000)) //设置token过期时间，当前时间加一天就是时效为一天过期
                 .signWith(SignatureAlgorithm.HS256, "ycj123456")     //签名部分，设置HS256加密方式和加密密码,ycj123456是自定义的密码
