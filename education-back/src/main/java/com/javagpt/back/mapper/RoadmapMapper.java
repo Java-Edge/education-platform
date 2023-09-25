eb0391cbe51d4113411008847e3697b0ac9974c7
Li Deng <li.deng129@gamil.com>
2023-09-25 23:38:26 +1000

    feat: roadmap

---
+++ education-back/src/main/java/com/javagpt/back/mapper/RoadmapMapper.java
@@ -0,0 +1,10 @@
+package com.javagpt.back.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.javagpt.back.entity.Roadmap;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RoadmapMapper extends BaseMapper<Roadmap> {
+
+}
