28260033d8ed10e5bdb57f5097d2b2d8955d5a97
JavaEdge <sss2855845850@gmail.com>
2023-10-13 18:07:52 +0800

    refactor:移除无用代码

--- education-back/src/main/java/com/javagpt/back/service/RankingService.java
+++ education-back/src/main/java/com/javagpt/back/service/RankingService.java
@@ -1,16 +1,10 @@
 package com.javagpt.back.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.javagpt.back.entity.ArticleEntity;
 import com.javagpt.back.entity.Ranking;
 
 import java.util.List;
 
-/**
-* @author MSIK
-* @description 针对表【source_course】的数据库操作Service
-* @createDate 2023-07-09 13:40:08
-*/
 public interface RankingService extends IService<Ranking> {
 
     List<Ranking> getRanking();
--- education-back/src/main/java/com/javagpt/back/service/impl/RankingServiceImpl.java
+++ education-back/src/main/java/com/javagpt/back/service/impl/RankingServiceImpl.java
@@ -2,13 +2,9 @@ package com.javagpt.back.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.javagpt.back.entity.ArticleEntity;
 import com.javagpt.back.entity.Ranking;
-import com.javagpt.back.mapper.ArticleMapper;
 import com.javagpt.back.mapper.RankingMapper;
-import com.javagpt.back.service.ArticleService;
 import com.javagpt.back.service.RankingService;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
