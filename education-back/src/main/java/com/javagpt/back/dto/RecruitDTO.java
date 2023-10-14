2e7342ceadf50b145e54ad5e2259ceb92f4a8e20
JavaGPT <2631416434@qq.com>
2023-10-14 17:30:25 +0800

    refactor： 传输对象删除无用参数

--- education-back/src/main/java/com/javagpt/back/dto/RecruitDTO.java
+++ education-back/src/main/java/com/javagpt/back/dto/RecruitDTO.java
@@ -24,34 +24,9 @@ public class RecruitDTO implements Serializable {
     private Integer type;
 
     /**
-     * 岗位描述
-     */
-    private String des;
-
-    /**
-     * 联系电话
-     */
-    private String phone;
-
-    /**
-     * 公司编号
-     */
-    private Integer companyId;
-
-    /**
      * 招聘类型 校招、实习、社招
      */
     private Integer recruitType;
-
-    /**
-     * 招聘名称
-     */
-    private String jobName;
-
-    /**
-     * 额外信息
-     */
-    private String ext;
 
     /**
      * 工作城市
@@ -80,51 +55,6 @@ public class RecruitDTO implements Serializable {
 
 
     /**
-     * 招聘开始时间
-     */
-    private Date deliverBegin;
-
-    /**
-     * 招聘结束时间
-     */
-    private Date deliverEnd;
-
-    /**
-     * 刷新时间
-     */
-    private Date refeshTime;
-
-    /**
-     * 
-     */
-    private Integer feedBackDays;
-
-    /**
-     * 
-     */
-    private Integer durationDays;
-
-    /**
-     * 薪资范围
-     */
-    private Integer salaryRange;
-
-    /**
-     * 薪水类型
-     */
-    private Integer salaryType;
-
-    /**
-     * 薪资最小值
-     */
-    private Integer salaryMin;
-
-    /**
-     * 薪资最大值
-     */
-    private Integer salaryMax;
-
-    /**
      * 
      */
     private Integer salaryMonth;
@@ -133,11 +63,6 @@ public class RecruitDTO implements Serializable {
      * 
      */
     private Integer workYearType;
-
-    /**
-     * 项目编号
-     */
-    private Integer projectId;
 
     /**
      * 学历要求
