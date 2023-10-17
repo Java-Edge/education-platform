bf6905f90230651886596e596d01fe21f86c17db
JavaEdge <sss2855845850@gmail.com>
2023-10-17 17:50:14 +0800

    fix:修正公司列表接口默认只取私企

--- education-back/src/main/java/com/javagpt/back/service/impl/CompanyServiceImpl.java
+++ education-back/src/main/java/com/javagpt/back/service/impl/CompanyServiceImpl.java
@@ -2,11 +2,10 @@ package com.javagpt.back.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.javagpt.back.entity.Company;
-import com.javagpt.back.entity.CourseCategory;
 import com.javagpt.back.mapper.CompanyMapper;
 import com.javagpt.back.service.CompanyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.javagpt.back.vo.course.CourseCategoryVO;
+import com.javagpt.common.enums.CompanyNatureEnums;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +16,7 @@ public class CompanyServiceImpl extends ServiceImpl<CompanyMapper, Company> impl
     @Override
     public List<String> getCompanyNames() {
         QueryWrapper<Company> qw = new QueryWrapper<>();
+        qw.eq("company_nature_id", CompanyNatureEnums.ENTERPRIZE.getCode());
         qw.select("company_name");
         List<Company> courseCategories = this.getBaseMapper().selectList(qw);
         return courseCategories.stream()
