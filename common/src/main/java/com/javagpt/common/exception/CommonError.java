378924ddea3a15241fffd7af06d87b97f26c6252
JavaGPT <2631416434@qq.com>
2023-10-18 21:44:01 +0800

    feat: 加入全局异常处理机制

---
+++ common/src/main/java/com/javagpt/common/exception/CommonError.java
@@ -0,0 +1,28 @@
+package com.javagpt.common.exception;
+
+
+import com.javagpt.common.constant.SystemConstant;
+
+public enum CommonError {
+
+    PARAM_INVALID(SystemConstant.PARAMETER_INVALID_CODE, "缺少必要參數")
+
+    ;
+
+
+    private final int code;
+    private final String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    CommonError(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
