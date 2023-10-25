c3269098024ff9a20187e1e4410cded122b64fe1
JavaEdge <sss2855845850@gmail.com>
2023-10-25 09:33:51 +0800

    refactor:重构动态控制器的命名

--- education-back/src/main/java/com/javagpt/back/controller/TimeLineController.java
+++ education-back/src/main/java/com/javagpt/back/controller/TimeLineController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @RestController
 @RequestMapping("/article")
-public class ArticleController {
+public class TimeLineController {
 
     @Autowired
     private ArticleService articleService;
