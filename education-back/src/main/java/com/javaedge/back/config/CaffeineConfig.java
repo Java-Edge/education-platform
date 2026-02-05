7382aabfabb7498d9a6a5a7195f38caf1de389fa
JavaEdge <sss2855845850@gmail.com>
2026-02-05 23:11:42 +0800

    fix:面经不适合缓存

--- education-back/src/main/java/com/javaedge/back/config/CaffeineConfig.java
+++ education-back/src/main/java/com/javaedge/back/config/CaffeineConfig.java
@@ -1,11 +1,9 @@
 package com.javaedge.back.config;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.javaedge.back.entity.Dictionary;
 import com.javaedge.back.entity.DictionaryType;
-import com.javaedge.back.entity.InterviewEntity;
 import com.javaedge.back.entity.Pilot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -58,34 +56,6 @@ public class CaffeineConfig {
         return Caffeine.newBuilder()
                 .maximumSize(1)
                 .expireAfterWrite(20, TimeUnit.MINUTES)
-                .build();
-    }
-
-    /**
-     * 面经分页查询缓存
-     * 缓存键格式：pageNo-pageSize-jobId
-     */
-    @Bean
-    public Cache<String, IPage<InterviewEntity>> interviewPageCache() {
-        return Caffeine.newBuilder()
-                // 缓存最多100个不同的分页查询结果
-                .maximumSize(100)
-                // 5分钟后过期，确保数据相对新鲜
-                .expireAfterWrite(5, TimeUnit.MINUTES)
-                .build();
-    }
-
-    /**
-     * 面经详情缓存
-     * 缓存键：面经ID
-     */
-    @Bean
-    public Cache<String, InterviewEntity> interviewDetailCache() {
-        return Caffeine.newBuilder()
-                // 缓存最多500个面经详情
-                .maximumSize(500)
-                // 10分钟后过期
-                .expireAfterWrite(10, TimeUnit.MINUTES)
                 .build();
     }
 }
--- education-back/src/main/java/com/javaedge/back/service/impl/InterviewServiceImpl.java
+++ education-back/src/main/java/com/javaedge/back/service/impl/InterviewServiceImpl.java
@@ -3,7 +3,6 @@ package com.javaedge.back.service.impl;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.github.benmanes.caffeine.cache.Cache;
 import com.javaedge.back.dto.InterviewDTO;
 import com.javaedge.back.entity.Career;
 import com.javaedge.back.entity.InterviewEntity;
@@ -27,11 +26,6 @@ public class InterviewServiceImpl extends ServiceImpl<InterviewMapper, Interview
     @Autowired
     private CareerMapper careerMapper;
 
-    @Autowired
-    private Cache<String, IPage<InterviewEntity>> interviewPageCache;
-
-    @Autowired
-    private Cache<String, InterviewEntity> interviewDetailCache;
 
     @Override
     public IPage<InterviewEntity> selectByCondition(PageQueryParam<InterviewDTO> pageQueryParam) {
@@ -46,51 +40,22 @@ public class InterviewServiceImpl extends ServiceImpl<InterviewMapper, Interview
             pageQueryParam.setParam(new InterviewDTO());
         }
 
-        // 构建缓存键：pageNo-pageSize-jobId
-        Integer jobId = pageQueryParam.getParam().getJobId();
-        String cacheKey = String.format("%d-%d-%s",
-                pageQueryParam.getPageNo(),
-                pageQueryParam.getPageSize(),
-                jobId != null ? jobId : "all");
-
-        // 尝试从缓存获取
-        IPage<InterviewEntity> cachedResult = interviewPageCache.getIfPresent(cacheKey);
-        if (cachedResult != null) {
-            return cachedResult;
-        }
-
-        // 缓存未命中，执行数据库查询
+        // 执行数据库查询
         Page<InterviewEntity> interviewEntityPage = new Page<>();
         interviewEntityPage.setSize(pageQueryParam.getPageSize());
         interviewEntityPage.setCurrent(pageQueryParam.getPageNo());
 
-        IPage<InterviewEntity> result = interviewMapper.selectByCondition(pageQueryParam.getParam(), interviewEntityPage);
-
-        // 将结果放入缓存
-        interviewPageCache.put(cacheKey, result);
-
-        return result;
+        return interviewMapper.selectByCondition(pageQueryParam.getParam(), interviewEntityPage);
     }
 
     @Override
     public InterviewEntity articleOf(String id) {
-        // 尝试从缓存获取
-        InterviewEntity cachedArticle = interviewDetailCache.getIfPresent(id);
-        if (cachedArticle != null) {
-            return cachedArticle;
-        }
-
-        // 缓存未命中，执行数据库查询
         InterviewEntity article = getById(id);
         Integer jobId = article.getJobId();
         if (Objects.nonNull(jobId)) {
             Career career = careerMapper.selectById(jobId);
             article.setCareerName(career.getName());
         }
-
-        // 将结果放入缓存
-        interviewDetailCache.put(id, article);
-
         return article;
     }
 }
